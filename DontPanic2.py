e04b60f251444188b911907e4eed7545293610f6
Aleksey Berezan <aleksey.berezan@gmail.com>
2017-04-17 23:35:26 -0400

    DontPanic2.py passes all tests. Polishing left.

--- DontPanic2.py
+++ DontPanic2.py
@@ -32,16 +32,11 @@ def backtrack(finish_node):
         raise Exception("Unable to find optimal path")
     current = finish_node
     result = []
-    prev_pos = None
     while current != None:
-        match_floor = max(0, current.floor-1)
-        pos = prev_pos if prev_pos != None else current.pos
-        result.append((current.match_floor, current.match_pos, current.action, current.direction))
-
-        prev_pos = current.pos
+        result.append((current.match_floor, current.match_pos, current.action, current.distance))
         current = current.prev_node
 
-    return list(reversed(result))
+    return list(reversed(result))[1:]
 
 def filter_exits(pos, exits):
     # return exits
@@ -81,8 +76,11 @@ def find_path(start_floor, start_pos, exit_floor, exit_pos, floors_map, total_el
                 finish_node = current if distance < finish_node.distance else finish_node
             continue
 
-        # probe all exits 
+        # probe all exits
+
         exits = [exit_pos] if floor == exit_floor else (filter_exits(pos, floors_map[floor]) if floor in floors_map else [])
+        if pos in exits:
+            exits = [pos]
         for ex in exits:
             next_action, next_direction, next_distance = get_next_step(pos, direction, ex, distance)
             q.put(Node(elevators_left, next_distance, next_direction, next_action, floor + 1, ex, current, floor, pos))
@@ -95,7 +93,7 @@ def find_path(start_floor, start_pos, exit_floor, exit_pos, floors_map, total_el
         # probe additional elevators
         if elevators_left > 0:
             elevator_poss = [exit_pos]
-            for ex in exits:
+            for ex in filter_exits(pos, exits):
                 elevator_poss.append(ex - 1)
                 elevator_poss.append(ex + 1)
             elevator_poss = list(set(elevator_poss))
@@ -114,51 +112,26 @@ def find_path(start_floor, start_pos, exit_floor, exit_pos, floors_map, total_el
             for elevator_pos in elevator_poss:
                 next_action, next_direction, next_distance = get_next_step(pos, direction, elevator_pos, distance)
                 walk = Node(elevators_left, next_distance, next_direction, next_action, floor + 1, elevator_pos, current, floor, pos)
-                if walk.action != "WAIT":
-                    # q.put(walk)
-                    q.put(Node(elevators_left - 1, walk.distance + 3, direction, "ELEVATOR", floor + 1, elevator_pos, walk, floor, elevator_pos))
-                else:
-                    q.put(Node(elevators_left - 1, distance + 3, direction, "ELEVATOR", floor + 1, elevator_pos, current, floor, elevator_pos))
+                q.put(Node(elevators_left - 1, walk.distance + 3, walk.direction, "ELEVATOR", floor + 1, elevator_pos, walk, floor, elevator_pos))
 
     # backtrack
-    debug("backtracking")
-    return backtrack(finish_node)[1:]
+    return backtrack(finish_node)
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
-
-# nb_floors: number of floors
-# width: width of the area
-# nb_rounds: maximum number of rounds
-# exit_floor: floor on which the exit is found
-# exit_pos: position of the exit on its floor
-# nb_total_clones: number of generated clones
-# nb_additional_elevators: number of additional elevators that you can build
-# nb_elevators: number of elevators
 nb_floors, width, nb_rounds, exit_floor, exit_pos, nb_total_clones, nb_additional_elevators, nb_elevators = [int(i) for i in raw_input().split()]
 floors_map = { }
 for i in xrange(nb_elevators):
-    # elevator_floor: floor on which this elevator is found
-    # elevator_pos: position of the elevator on its floor
     elevator_floor, elevator_pos = [int(j) for j in raw_input().split()]
     if elevator_floor in floors_map:
         floors_map[elevator_floor].append(elevator_pos)
     else:
         floors_map[elevator_floor] = [elevator_pos]
 
-debug(nb_elevators)
-debug(nb_additional_elevators)
-debug(nb_rounds)
-elevators_built = 0
-debug("exit: " + str(exit_floor))
-
 optimal_path = None
 
 # game loop
 while True:
-    # clone_floor: floor of the leading clone
-    # clone_pos: position of the leading clone on its floor
-    # direction: direction of the leading clone: LEFT or RIGHT
     clone_floor, clone_pos, direction = raw_input().split()
     clone_floor = int(clone_floor)
     clone_pos = int(clone_pos)
@@ -167,19 +140,16 @@ while True:
         optimal_path = find_path(clone_floor, clone_pos, exit_floor, exit_pos, floors_map, nb_additional_elevators)
         debug("Calculated optimal path to: " + str(optimal_path))
     
-    debug("Leader at: {0}/{1} and moving to {2}".format(clone_floor, clone_pos, direction))
-
     if direction == "NONE" or len(optimal_path) == 0:
-        debug("Direction is None, waiting")
         print "WAIT"
         continue
 
-    (floor, pos, action, current_direction) = optimal_path[0]
+    (floor, pos, action, distance) = optimal_path[0]
     if clone_floor == floor and clone_pos == pos:
+        debug("Executing and deleting action {0}".format(action))
         del optimal_path[0]
-        debug("Executing and deleting action {0} with expected direction {1}".format(action, current_direction))
         print action
     else:
-        debug("gonna wait")
         print "WAIT"
-    debug("Optimal path is: " + str(optimal_path))
+
+    debug("Left to go: {0}".format(optimal_path))
